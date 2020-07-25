2a8a86d3f73179b4a8475c48d1a1483dabe6b9ae
AliceAAtalla <aliceatalla36@gmail.com>
2020-07-25 16:24:50 -0300

    Updates

---
+++ precache-manifest.0208c5dddb0590cbf4c43bce73747f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b7831fbc827e17217970d34c536e0b",
+    "url": "/FirstCalculator/index.html"
+  },
+  {
+    "revision": "c8d0e5d0e83573641087",
+    "url": "/FirstCalculator/static/css/main.489b9038.chunk.css"
+  },
+  {
+    "revision": "3f55dbd2631213aac5bf",
+    "url": "/FirstCalculator/static/js/2.eb1c8e8f.chunk.js"
+  },
+  {
+    "revision": "c8d0e5d0e83573641087",
+    "url": "/FirstCalculator/static/js/main.755f014d.chunk.js"
+  },
+  {
+    "revision": "796b2a82f2892bc711ba",
+    "url": "/FirstCalculator/static/js/runtime~main.e27a893c.js"
+  },
+  {
+    "revision": "7cb58857d294ac1e09b72ea9403c690a",
+    "url": "/FirstCalculator/static/media/RobotoMono-Thin.7cb58857.ttf"
+  }
+]);
